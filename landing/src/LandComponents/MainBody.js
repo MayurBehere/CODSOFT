03e74cbabed18caae491034bb16cb676542c62b2
Mayur <mayurbehere7432@gmail.com>
2023-11-09 18:36:37 +0530

    Add Hero component to MainBody and update Navbar button styles

--- landing/src/LandComponents/MainBody.js
+++ landing/src/LandComponents/MainBody.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Hero from './Hero'
 import './MainBody.css'
 
 const MainBody = () => {
   return (
+    <>
     <div className='Main-Container'>
       <Navbar/>
     </div>
-  )
+    <div className='hero'>
+      <Hero/>
+    </div>
+  </>)
 }
 
 export default MainBody
